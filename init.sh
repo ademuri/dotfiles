ae35e1f506d2a254114e17803242d0e01bef6c34
Adam Demuri <adam.demuri@gmail.com>
2019-12-10 20:47:48 -0700

    Add replace_cpp

--- init.sh
+++ init.sh
@@ -6,3 +6,9 @@ ln -i -s $DIR/vimrc ~/.vimrc
 ln -i -s $DIR/tmux.conf ~/.tmux.conf
 ln -i -s $DIR/gitconfig ~/.gitconfig
 ln -i -s $DIR/inputrc ~/.inputrc
+
+echo "Installing bin..."
+mkdir -p "$HOME/bin"
+for f in bin/*; do
+  ln -s "$PWD/$f" "$HOME/bin/"
+done
