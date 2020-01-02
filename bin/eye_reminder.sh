a18063d4c992fdf8f5c3ef99789b972eb1557c90
Adam Demuri <adam.demuri@gmail.com>
2020-01-02 14:46:48 -0700

    Only send eye notification if screen isn't locked

--- bin/eye_reminder.sh
+++ bin/eye_reminder.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)";
+
+# Only send notification if the screen isn't locked
+logged_in=$(loginctl show-user adam --property=IdleHint)
+if [[ "$logged_in" == "IdleHint=yes" ]]; then
+  exit 0
+fi
+
 /usr/bin/notify-send "Eye break                       " "Take a break!\
   \
   \
   \
   " -t 22000
-echo "run" >> /home/adam/run
