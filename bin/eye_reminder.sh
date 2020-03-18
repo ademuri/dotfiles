dcad81b37db8ea4dbc502c50869d7f3c888d95ae
Adam Demuri <adam.demuri@gmail.com>
2020-03-17 20:14:14 -0600

    Fix eye reminder to work with ubuntu 19.04

--- bin/eye_reminder.sh
+++ bin/eye_reminder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)";
+eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -o -u $LOGNAME gnome-session)/environ)";
 
 # Only send notification if the screen isn't locked
 logged_in=$(loginctl show-user adam --property=IdleHint)
