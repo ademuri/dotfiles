94ab31ce302774e3daa6825fb47399f813acdcab
Adam DeMuri <adam.demuri@gmail.com>
2023-11-03 08:36:42 -0600

    Add vertical mouse button config

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ ln -i -s $DIR/vimrc ~/.vimrc
 ln -i -s $DIR/tmux.conf ~/.tmux.conf
 ln -i -s $DIR/gitconfig ~/.gitconfig
 ln -i -s $DIR/inputrc ~/.inputrc
+sudo ln -i -s $DIR/90-evoluent.conf /usr/share/X11/xorg.conf.d/
 
 echo "Installing bin..."
 mkdir -p "$HOME/bin"
